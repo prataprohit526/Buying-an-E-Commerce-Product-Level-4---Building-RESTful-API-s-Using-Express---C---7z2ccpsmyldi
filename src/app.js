2d0dcd4ce83fe75b00a80019a4ccbf27fee9e433
prataprohit526 <prataprohit526@gmail.com>
2023-04-21 14:54:08 +0530

    add all

--- src/app.js
+++ src/app.js
@@ -1,21 +1,37 @@
-const fs = require('fs');
-const express = require('express');
+const fs = require("fs");
+const express = require("express");
 const app = express();
 
-
 // Importing products from products.json file
-const products = JSON.parse(
-    fs.readFileSync(`${__dirname}/data/product.json`)
-);
-
+const products = JSON.parse(fs.readFileSync(`${__dirname}/data/product.json`));
 
 // Middlewares
 app.use(express.json());
 
+const patchMiddleware = (req, res, next) => {
+  const id = +req.params.id;
+  const fetchData = products.find((el) => el.id === id);
+  if (!fetchData) {
+    return res
+      .status(404)
+      .json({ status: "failed", message: "Product not found!" });
+  } else if (fetchData.quantity < 1) {
+    return res.status(404).json({
+      status: "success",
+      message: `${fetchData.name} , Out of stock!`,
+    });
+  }
+  res.json({
+    status: "success",
+    message: `Thank you for purchasing ${fetchData.name}`,
+    product: fetchData,
+  });
+  console.log(id);
+  next();
+};
+
 // Write PATCH endpoint to buy a product for the client here
+app.patch("/api/v1/products/:id", patchMiddleware);
 // Endpoint /api/v1/products/:id
-
-
-
 
 module.exports = app;
